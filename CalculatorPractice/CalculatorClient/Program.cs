6e61b6f54b4d03c11c0acce8b515b436f83c2ddd
kkimkkom <kkimkkomca@gmail.com>
2022-10-04 19:31:06 -0400

    Added more method

--- CalculatorPractice/CalculatorClient/Program.cs
+++ CalculatorPractice/CalculatorClient/Program.cs
@@ -12,6 +12,8 @@ namespace CalculatorClient
         {
             CalculatorReference.CalculatorServiceClient client = new CalculatorReference.CalculatorServiceClient();
             string choice = "";
+            double n1 = 0;
+            double n2 = 0;
             while (!choice.Equals("5"))
             {
                 Console.WriteLine("\t\tCalculator");
@@ -27,24 +29,40 @@ namespace CalculatorClient
                 {
                     case "1":
                         Console.Write("Enter Number 1: ");
-                        double n1 = double.Parse(Console.ReadLine());
+                        n1 = double.Parse(Console.ReadLine());
                         Console.Write("Enter Number 2: ");
-                        double n2 = double.Parse(Console.ReadLine());
+                        n2 = double.Parse(Console.ReadLine());
                         Console.WriteLine("The sum is : " + client.Add(n1,n2));
                         break;
+
                     case "2":
+                        Console.Write("Enter Number 1: ");
+                        n1 = double.Parse(Console.ReadLine());
+                        Console.Write("Enter Number 2: ");
+                        n2 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("The sub is : " + client.Sub(n1, n2));
                         break;
+
                     case "3":
+                        Console.Write("Enter Number 1: ");
+                        n1 = double.Parse(Console.ReadLine());
+                        Console.Write("Enter Number 2: ");
+                        n2 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("The Mul is : " + client.Mul(n1, n2));
                         break;
+
                     case "4":
+                        Console.Write("Enter Number 1: ");
+                        n1 = double.Parse(Console.ReadLine());
+                        Console.Write("Enter Number 2: ");
+                        n2 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("The Div is : " + client.Div(n1, n2));
                         break;
 
                     default:
                         break;
                 }
             }
-
-
             Console.ReadKey();
 
         }
