6875defc65627bdae706adee4a8fb2c4be325af1
kkimkkom <kkimkkomca@gmail.com>
2022-10-04 20:01:43 -0400

    Login Practice

---
+++ LoginPractice/LoginClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoginReference.LoginClient client = new LoginReference.LoginClient();
+            Console.Write("Enter email: ");
+            string user = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string pass = Console.ReadLine();
+
+            string res = client.Login(user, pass);
+            Console.WriteLine(res);
+            Console.ReadLine();
+            Console.ReadKey();
+            
+        }
+    }
+}
